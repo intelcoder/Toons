b101a27252e8dbd242f3030cbbc9e7c8811f10c7
intelcoder <seongjunkim7162@gmail.com>
2017-06-11 12:29:50 -0400

    WebtoonPager added

--- src/screens/WebtoonScreen/WebtoonScreen.js
+++ src/screens/WebtoonScreen/WebtoonScreen.js
@@ -2,36 +2,56 @@
  * Created by fiddlest on 5/22/2017.
  */
 import React, { Component } from 'react'
-import { View, Text, Button, AsyncStorage } from 'react-native'
+import { View, Text, Button, AsyncStorage,Dimensions } from 'react-native'
 import {connect} from 'react-redux'
 import { bindActionCreators } from 'redux'
 import {requestReadPermission, requestWritePermission} from 'utils/permissionRequest'
 import {INIT_START, FETCH_WEBTOON_DB} from 'redux/types'
-import {fetchWebtoonFromDb, initStart} from 'redux/actions'
+import {updateSite, initStart} from 'redux/actions'
 import {defaultModel} from 'models/model'
+import {WebtoonPager} from 'components'
+
+import { createSelector } from 'reselect'
 
 @connect((state) => ({
   login: state.login,
-  init: state.init
+  init: state.init,
+  site: state.webtoon.site
 }),
 (dispatch) => {
   return bindActionCreators({
       startInit: initStart,
-      fetchWebtoonFromDb: fetchWebtoonFromDb
+      updateSite: updateSite
   }, dispatch)
 })
 class WebtoonScreen extends Component {
 
+  state = {
+    width: 0,
+    height: 0,
+  }
+
+   componentWillMount() {
+    const {width, height} = Dimensions.get('window');
+    this.setState({
+      width: width,
+      height: height
+    });
+  }
   componentDidMount() {
     requestReadPermission()
     requestWritePermission()
+
     this.initOrFetchStart();
   }
+
   initOrFetchStart = async () => {
+    const {site} = this.props
+
     //Later on naver will be fetched from user config
-    const naverToonIds = await defaultModel.getByKey('naver')
+    const naverToonIds = await defaultModel.getByKey(site)
     if(naverToonIds){
-       this.props.fetchWebtoonFromDb('naver')
+       this.props.updateSite(site)
     }else {
       this.props.startInit()
     }
@@ -39,20 +59,9 @@ class WebtoonScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Webtoon Screen</Text>
-        <Button
-          onPress={() => {
-            this.props.startInit()
-          }}
-          title="Start init ~!!!!"
-        />
-
-          <Button
-          onPress={() => {
-            console.log('WebtoonScreen',this.props)
-          }}
-          title={this.props.init.status ? this.props.init.status : 'show me props' }
+      <View style={{flex: 1}}>
+        <WebtoonPager 
+          width={this.state.width}
         />
       </View>
     )
