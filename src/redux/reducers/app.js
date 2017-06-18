d88d0191a644fd06d0db72027d2d6e9a0186d13f
intelcoder <seongjunkim7162@gmail.com>
2017-06-18 16:23:00 -0400

    App reducer added

---
+++ src/redux/reducers/app.js
@@ -0,0 +1,14 @@
+import { createReducer } from 'utils/reduxGenerator'
+
+const initState = {
+  isConnected: false,
+}
+
+const appReducer = createReducer(initState, {
+  ['UPDATE_CONNECT_STATE'](state, action) {
+    return {
+      ...state,
+      isConnected: action.newConnect,
+    }
+  },
+})
