27bc39ed03ba36bedc4740f2f97e41c737edfea6
intelcoder <seongjunkim7162@gmail.com>
2017-06-18 16:18:24 -0400

    Episode related actions added

--- src/redux/actions/webtoonActions.js
+++ src/redux/actions/webtoonActions.js
@@ -1,5 +1,13 @@
 import { makeActionCreator } from 'utils/reduxGenerator'
-import { SITE_UPDATE, SITE_UPDATE_SUCCESS } from 'redux/types'
+import {
+  SITE_UPDATE,
+  SITE_UPDATE_SUCCESS,
+  GET_EPISODES_DB,
+  GET_EPISODES_DB_SUCCESS,
+  GET_EPISODES_DB_FAIL,
+  GET_EPISODES_API,
+  WEBTOON_SELECTED,
+} from 'redux/types'
 
 export const updateSite = makeActionCreator(SITE_UPDATE, 'site')
 export const siteUpdated = makeActionCreator(
@@ -7,3 +15,17 @@ export const siteUpdated = makeActionCreator(
   'site',
   'webtoons'
 )
+
+export const getEpisodesDb = makeActionCreator(GET_EPISODES_DB, 'episodeKeys')
+export const getEpisodesDbSuccess = makeActionCreator(
+  GET_EPISODES_DB_SUCCESS,
+  'episodes'
+)
+
+export const getEpisodesApi = makeActionCreator(
+  GET_EPISODES_API,
+  'toonId',
+  'episodeKey'
+)
+
+export const setWebtoonId = makeActionCreator(WEBTOON_SELECTED, 'toonId')
