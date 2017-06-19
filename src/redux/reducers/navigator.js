260b46e7811652df1e564ae23d2fb39b8d43c88c
intelcoder <seongjunkim7162@gmail.com>
2017-06-18 21:55:18 -0400

    Nav updated

--- src/redux/reducers/navigator.js
+++ src/redux/reducers/navigator.js
@@ -2,16 +2,15 @@
  * Created by fiddlest on 5/22/2017.
  */
 
-import {AppNavigator} from '../../navigators/AppNavigator/AppNavigator';
+import { AppNavigator } from '../../navigators/AppNavigator/AppNavigator'
 
-const homeAction = AppNavigator.router.getActionForPathAndParams('Login');
-const initialState = AppNavigator.router.getStateForAction(homeAction);
+const homeAction = AppNavigator.router.getActionForPathAndParams('Webtoon')
+const initialState = AppNavigator.router.getStateForAction(homeAction)
 
 const navReducer = (state = initialState, action) => {
-  const nextState = AppNavigator.router.getStateForAction(action, state);
+  const nextState = AppNavigator.router.getStateForAction(action, state)
 
-  return nextState || state;
-};
+  return nextState || state
+}
 
-
-export default navReducer;
+export default navReducer
