54ec71880c6d2abd3442ef068124afe20a57976a
intelcoder <seongjunkim7162@gmail.com>
2017-06-18 21:54:16 -0400

    reformatted

--- src/components/EpisodeList/index.js
+++ src/components/EpisodeList/index.js
@@ -2,50 +2,39 @@
  * Created by fiddlest on 3/12/2017.
  * @flow
  */
-'use strict';
-import React, {Component} from 'react';
-import {
-  View,
-  Text,
-  ListView,
-  ToolbarAndroid,
-  StyleSheet
-} from 'react-native';
-import EpisodeBox from 'components/EpisodeBox';
+'use strict'
+import React, { Component } from 'react'
+import { View, Text, ListView, ToolbarAndroid, StyleSheet } from 'react-native'
+import EpisodeBox from 'components/EpisodeBox'
 
 const styles = StyleSheet.create({
-  episodeList: {}
-});
-
+  episodeList: {},
+})
 
 export default class EpisodeList extends Component {
-  state:{
-    episodes: []
-  };
-
   constructor(props) {
-    super(props);
-    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+    super(props)
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 })
     this.state = {
-      episodes: this.ds.cloneWithRows(this.props.episodes)
+      episodes: this.ds.cloneWithRows(this.props.episodes),
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.episodes !== nextProps.episodes) {
       this.setState({
-        episodes: this.ds.cloneWithRows(nextProps.episodes)
+        episodes: this.ds.cloneWithRows(nextProps.episodes),
       })
     }
   }
 
   render() {
-    const {width, height, handleClick} = this.props;
+    const { width, height, handleClick } = this.props
     return (
       <View style={styles.episodeList}>
         <ListView
           dataSource={this.state.episodes}
-          renderRow={(episode)=> {
+          renderRow={episode => {
             return (
               <EpisodeBox
                 width={width}
@@ -53,7 +42,6 @@ export default class EpisodeList extends Component {
                 episode={episode}
                 handleClick={handleClick}
               />
-
             )
           }}
         />
@@ -61,4 +49,3 @@ export default class EpisodeList extends Component {
     )
   }
 }
-
