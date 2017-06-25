86d7d60049bbd05ef68f0261f58e3f43287cdb68
intelcoder <seongjunkim7162@gmail.com>
2017-06-25 13:51:30 -0400

    app added to reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,10 +6,12 @@ import navReducer from './navigator'
 import loginReducer from './loginReducer'
 import initReducer from './initReducer'
 import webtoonReducer from './webtoonReducer'
+import app from './app'
 
 export default combineReducers({
   nav: navReducer,
   login: loginReducer,
   init: initReducer,
   webtoon: webtoonReducer,
+  app: app
 })
