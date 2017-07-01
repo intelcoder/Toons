3f526a037c3d2f25b46858095612c96e67b1582e
intelcoder <seongjunkim7162@gmail.com>
2017-07-01 15:43:34 -0400

    Added baseurl

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as WebtoonPager } from './WebtoonPager'
 export { default as BasicSpinner } from './BasicSpinner'
+export { default as BaseUrlInputModal } from './BaseUrlInputModal'
