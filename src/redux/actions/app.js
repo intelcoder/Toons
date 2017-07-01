021d698692fa964662b8fbfc5c7caea543b4876c
intelcoder <seongjunkim7162@gmail.com>
2017-07-01 15:43:11 -0400

    toggle modal action, type added

--- src/redux/actions/app.js
+++ src/redux/actions/app.js
@@ -2,3 +2,6 @@ import { makeActionCreator } from 'utils/reduxGenerator'
 
 export const TOGGLE_LIKE = 'TOGGLE_LIKE'
 export const activateLike = makeActionCreator(TOGGLE_LIKE)
+
+export const TOGGLE_BASE_URL_MODAL = 'TOGGLE_BASE_URL_MODAL'
+export const toggleBaseUrlModal = makeActionCreator(TOGGLE_BASE_URL_MODAL)
