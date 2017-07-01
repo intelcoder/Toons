e9eb110a896fda6ec00ee0b4e2c809d8d3e68b19
intelcoder <seongjunkim7162@gmail.com>
2017-07-01 15:44:21 -0400

    toggle base url added to reducer

--- src/redux/reducers/app.js
+++ src/redux/reducers/app.js
@@ -1,8 +1,9 @@
 import { createReducer } from 'utils/reduxGenerator'
-import { TOGGLE_LIKE } from 'redux/actions'
+import { TOGGLE_LIKE, TOGGLE_BASE_URL_MODAL } from 'redux/actions'
 const initState = {
   isConnected: false,
   likeActivated: false,
+  showBaseUrlModal: false,
 }
 
 const appReducer = createReducer(initState, {
@@ -16,6 +17,12 @@ const appReducer = createReducer(initState, {
     return {
       ...state,
       likeActivated: !state.likeActivated,
+    }
+  },
+  [TOGGLE_BASE_URL_MODAL](state, action) {
+    return {
+      ...state,
+      showBaseUrlModal: !state.showBaseUrlModal,
     }
   },
 })
